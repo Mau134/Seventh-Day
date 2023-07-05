2f86ca5766b8724e6717c9956573f88390365fd0
Maurice Molande <molande.mau@gmail.com>
2023-07-05 18:28:41 +0200

    seventh day version 2

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 const nav = document.getElementById('mynav');
 
-window.onscroll = () => {
-    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-        nav.style.backgroundColor = '#7bb2c2'
-    }
-    else {
-        nav.style.backgroundColor = '#ffffff00'
-    }
 
-};
